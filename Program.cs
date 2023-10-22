7a38c22754e9fb908e9b4eaacdb4738899acdc63
ectf15 <ectineof@gmail.com>
2023-10-22 14:51:28 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+
+for (int j = 0; j < 1000; j++)
+{
+
+    Random numRandom = new Random();
+    string numeroTarjeta = "" + numRandom.Next(4,5);
+    for (int i = 0; i < 16; i++)
+    {
+      int digito = numRandom.Next(10); 
+      numeroTarjeta += digito;
+    }
+
+    Console.WriteLine("TDC: " + numeroTarjeta);
+    _ = ProcesarPago(numeroTarjeta);
+}
+Stopwatch sw = Stopwatch.StartNew();
+await Task.Delay(10000);
+sw.Stop();
+Console.WriteLine($"El proceso duró {TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds).TotalSeconds} segundos en completarse");
+async Task ProcesarPago(string numTarjeta)
+{
+    await Task.Delay(10000);
+}
+
